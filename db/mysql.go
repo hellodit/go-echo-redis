a49ef87422b5074b037c978ce9b9e04e48e0a001
hellodit <asditap@gmail.com>
2022-08-04 21:37:35 +0700

    enhance: change env variable to perform heroku env

--- db/mysql.go
+++ db/mysql.go
@@ -1,24 +1,16 @@
 package db
 
 import (
-	"fmt"
-
 	"github.com/spf13/viper"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectMysql() *gorm.DB {
-	var dbHost = viper.GetString("DB_HOST")
-	var dbPort = viper.GetString("DB_PORT")
-	var dbUser = viper.GetString("DB_USER")
-	var dbPass = viper.GetString("DB_PASSWORD")
-	var dbName = viper.GetString("DB_NAME")
 
-	parse := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		dbUser, dbPass, dbHost, dbPort, dbName)
+	connection := viper.GetString("DATABASE_URL")
 
-	db, err := gorm.Open(mysql.Open(parse), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.16
 require (
 	github.com/go-redis/redis/v7 v7.4.0
 	github.com/labstack/echo/v4 v4.2.2
-	github.com/sirupsen/logrus v1.2.0
+	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.7.1
-	gorm.io/driver/mysql v1.0.5
-	gorm.io/gorm v1.21.3
+	gorm.io/driver/postgres v1.3.8
+	gorm.io/gorm v1.23.6
 )
